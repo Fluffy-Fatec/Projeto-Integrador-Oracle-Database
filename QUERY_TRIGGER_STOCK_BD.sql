e0eaaaecd2275a8c709c08cca1266267c4507c98
Victor Fernandes <victor.santos136@fatec.sp.gov.br>
2023-11-26 17:11:21 -0300

    trigger venda

--- QUERY_TRIGGER_STOCK_BD.sql
+++ QUERY_TRIGGER_STOCK_BD.sql
@@ -11,11 +11,30 @@ END;
 
 -- Trigger para decrementar o estoque após uma venda
 CREATE OR REPLACE TRIGGER after_venda
-AFTER INSERT ON Stock_Product
+AFTER INSERT ON product_command
 FOR EACH ROW
+DECLARE
+    quantity_sold INT;
+    product_id_sold INT;
+    amount_contained DECIMAL(10, 3);
 BEGIN
-    -- Atualiza o Stock_Product após a conclusão de um pedido de produto
-    UPDATE Stock
-    SET AMOUNT_AVAILBLE = AMOUNT_AVAILBLE - :NEW.SP_AMOUNT_CONTAINED
-    WHERE stock_id = :NEW.stock_id;
+    -- Verifica se o registro na tabela product_command foi inserido
+    IF :NEW.PC_QUANTITY IS NOT NULL THEN
+        -- Obtém a quantidade e o ID do produto vendido
+        quantity_sold := :NEW.PC_QUANTITY;
+        product_id_sold := :NEW.PRODUCT_ID;
+
+        -- Declara um cursor para percorrer os resultados da subconsulta
+        FOR stock_rec IN (
+            SELECT SP_AMOUNT_CONTAINED
+            FROM stock_product
+            WHERE PRODUCT_ID = product_id_sold
+        ) LOOP
+            -- Atualiza a quantidade disponível no estoque
+            UPDATE stock
+            SET AMOUNT_AVAILBLE = AMOUNT_AVAILBLE - (quantity_sold * stock_rec.SP_AMOUNT_CONTAINED)
+            WHERE STOCK_ID = product_id_sold;
+        END LOOP;
+    END IF;
 END;
+/
